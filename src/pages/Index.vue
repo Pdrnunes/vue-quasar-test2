639165a2a73b4b3497f13fc6dd39945949e515b1
Pdrnunes <bttnns@gmail.com>
2021-12-01 19:53:35 -0300

    adding list to display array items

---
+++ src/pages/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page padding>
+    <ul>
+      <li v-for="task in tasks" :key="task">
+        {{task}}
+      </li>
+    </ul>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tasks: ['Go to the market', 'buy crackers', 'buy butter']
+    }
+  }
+}
+</script>
+
+|<style>
+</style>
