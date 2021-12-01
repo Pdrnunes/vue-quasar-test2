db5d5f5d6e3f4afe7f5abe61bb89c8bfea4be7e6
Pdrnunes <bttnns@gmail.com>
2021-12-01 20:32:17 -0300

    Adding multliple instances of task property and handling them in the page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,10 @@
 <template>
   <q-page padding>
     <ul>
-      <li v-for="task in tasks" :key="task">
-        {{task}}
+      <li v-for="(task, index) in tasks" :key="task">
+        <div>{{task.name}} {{index}}</div>
+        <small>{{task.dueDate}} @ {{task.dueTime}}</small>
+        <button @click="deleteTask(index)">X</button>
       </li>
     </ul>
   </q-page>
@@ -12,7 +14,29 @@
 export default {
   data () {
     return {
-      tasks: ['Go to the market', 'buy crackers', 'buy butter']
+      tasks: [
+        {
+          name: 'Go to the market',
+          dueDate: '2021/12/01',
+          dueTime: '19:56'
+        },
+        {
+          name: 'Go to the bank',
+          dueDate: '2021/12/02',
+          dueTime: '14:00'
+        },
+        {
+          name: 'Go to the beach',
+          dueDate: '2021/12/03',
+          dueTime: '9:15'
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTask (index) {
+      this.tasks.splice(index, 1) /*  splice JSmethod removes an item from the array
+                                      by passing it's position(index) and how much items will be deleted */
     }
   }
 }
